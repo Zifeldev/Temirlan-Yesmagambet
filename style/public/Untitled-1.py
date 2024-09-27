d41591488feed342e530e955bb5cfe0dedec6c89
temirlan <tesmagambet2005@gmail.com>
2024-09-27 14:23:23 +0500

    first commit

--- style/public/Untitled-1.py
+++
@@ -1,7 +0,0 @@
-temperature = float(input("Enter the current temperature: "))
-while temperature > 102.5:
-    print("Temperature is too high! Turning down the thermostat...")
-    print("Waiting for 5 minutes...")
-    temperature = float(input("Enter the new temperature: "))
-
-print("Temperature is acceptable. Technician can check again in 15 minutes.") 
--- style/public/conversion_factor = 0.py
+++
@@ -1,5 +0,0 @@
-conversion_factor = 0.6214
-print(f"{'KPH':<10}{'MPH':<10}")
-for kph in range(60, 131, 10):
-    mph = kph * conversion_factor
-    print(f"{kph:<10}{mph:<10.1f}")
